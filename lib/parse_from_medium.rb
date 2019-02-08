0f25410ad7703ac5cf0bf4088ac08e7e7aa92bd8
Jenny Shih <jinghua.shih@gmai.com>
2019-02-08 12:05:35 +0800

    updated import style

--- lib/parse_from_medium.rb
+++ lib/parse_from_medium.rb
@@ -11,12 +11,24 @@ class Medium
       title: @homepage.xpath("//h1")[1].content
     }
 
-    last_post[:body] = Nokogiri::HTML(open(last_post[:url])).search('div.section-inner').tap do |body|
-      body.children[0..1].remove
-      body.children.remove_class
-    end
-
-    last_post[:subtitle] = last_post[:body].children[0].content    
+    last_post[:body] = clean(Nokogiri::HTML(open(last_post[:url])).search('div.section-inner'))
+    last_post[:subtitle] = last_post[:body].search('p').first.content  
     last_post  
   end
+
+  def clean post
+    post.children[0..1].remove
+    segments = post.children
+    segments.search('figure').each { |fig| clean_img(fig) }
+    segments.search('a em').remove
+    segments
+  end
+
+  private
+    def clean_img figure
+      img = figure.first_element_child.children.search('img')
+      figure.first_element_child.swap(img)
+    end
 end
+
+# Medium.new('jinghua.shih').last_post
