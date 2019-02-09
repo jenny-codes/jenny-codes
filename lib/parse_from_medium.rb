79595e8f1027341531fe96ed27e642baa33d93b3
Jenny Shih <jinghua.shih@gmai.com>
2019-02-09 09:34:23 +0800

    edited normalizing method

--- lib/parse_from_medium.rb
+++ lib/parse_from_medium.rb
@@ -7,26 +7,29 @@ class Medium
 
   def last_post
     last_post = {
-        url: @homepage.search('div.h a')[2].attribute('href').value,
-      title: @homepage.xpath("//h1")[1].content
+      medium_url: @homepage.search('div.h a')[2].attribute('href').value,
+           title: @homepage.xpath("//h1")[1].content
     }
 
-    last_post[:body] = clean(Nokogiri::HTML(open(last_post[:url])).search('div.section-inner'))
-    last_post[:subtitle] = last_post[:body].search('p').first.content  
+    last_post[:body] = normalize(Nokogiri::HTML(open(last_post[:medium_url])).search('div.section-inner'))
+    last_post[:description] = last_post[:body].search('p').first.content  
     last_post  
   end
 
-  def clean post
-    post.children[0..1].remove
-    segments = post.children
-    segments.search('figure').each { |fig| clean_img(fig) }
-    segments.search('a em').remove
-    segments
-  end
-
   private
-    def clean_img figure
-      img = figure.first_element_child.children.search('img')
+    def normalize post
+      post.children[0..1].remove
+      segments = post.children
+      segments.search('figure').each { |fig| normalize_img_block(fig) }
+      segments.search('a em').remove
+      segments
+    end
+
+    def normalize_img_block figure
+      img = figure.first_element_child.children.search('img')[0]
+      img['class'] = 'lazy'
+      img['data-src'] = img['src']
+      img.remove_attribute('src')
       figure.first_element_child.swap(img)
     end
 end
