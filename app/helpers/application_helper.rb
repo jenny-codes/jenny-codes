f4d81ea1d889cebfb904452e279e64959cd13317
Jenny Shih <jinghua.shih@gmai.com>
2019-02-09 09:14:13 +0800

    added flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,9 @@
 module ApplicationHelper
 
-  def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
-  end
-
   def flash_messages(opts = {})
     flash.each do |msg_type, msg|
       concat(
-        content_tag(:div, msg, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissable fade in") do
+        content_tag(:div, msg, class: "alert alert-#{msg_type} alert-dismissable fade-in") do
           concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
           concat msg.html_safe
         end
