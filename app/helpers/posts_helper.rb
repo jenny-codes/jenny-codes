e4aac7baf42af33bcf2f71d09a6b7b0d817db76f
Jenny Shih <jinghua.shih@gmai.com>
2019-02-17 12:33:24 +0800

    added medium_url inspection to post list

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def medium_url(post)
+    if post.medium_url
+      [URI.decode(post.medium_url).match(/(?<=shih\/).*/)[0].truncate(20), post.medium_url]
+    else
+      ["NO DATA"]
+    end
+  end
 end
