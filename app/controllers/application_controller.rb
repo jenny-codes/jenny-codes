77f55ce53a9055a6e62a50db0ec9ffdc589613c7
Jenny Shih <jinghua.shih@gmai.com>
2019-02-18 12:41:01 +0800

    custom domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   include Error::ErrorHandler
+  protect_from_forgery with: :exception
+  before_action :ensure_domain
+
+  private
+  def ensure_domain
+    if request.env['HTTP_HOST'] == "jennycodes.herokuapp.com"
+      redirect_to "https://jennycodes.me#{request.env['REQUEST_PATH']}", status: 301
+    end
+  end
+
 end
