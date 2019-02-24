1b4133ec1f9fc1a7760ccfab641ef6a323264055
Jenny Shih <jinghua.shih@gmai.com>
2019-02-24 08:02:39 +0800

    font adjustment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,10 +17,10 @@ Rails.application.configure do
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = false
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
+    # config.cache_store = :memory_store
+    # config.public_file_server.headers = {
+    #   'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    # }
   else
     config.action_controller.perform_caching = false
 
