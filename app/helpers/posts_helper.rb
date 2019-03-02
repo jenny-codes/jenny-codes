f73721d0a8b3a911c89f4abf4de306a3137a9f04
Jenny Shih <jinghua.shih@gmai.com>
2019-03-02 16:23:53 +0800

    fixed minor bug

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,11 @@
 module PostsHelper
   def medium_url(post)
     if post.medium_url
-      [URI.decode(post.medium_url).match(/(?<=shih\/).*/)[0].truncate(20), post.medium_url]
+      if url = URI.decode(post.medium_url).match(/(?<=shih\/).*/)
+        [url[0].truncate(20), post.medium_url]
+      else
+        ['WEIRD']
+      end
     else
       ["NO DATA"]
     end
