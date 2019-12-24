4d7c9eaf57a3a3060691c8b61ae07cbec91fe85c
jennyshih <jinghua.shih@gmail.com>
2019-12-24 20:20:48 +0800

    use Rails built-in validation method for Tag's text attribute

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,18 +2,11 @@ class Tag < ApplicationRecord
   has_many :taggings
   has_many :posts, through: :taggings
 
-  validate :text_should_be_present_and_unique
+  validates :text, presence: true, uniqueness: { case_sensitive: false }
 
   def self.from_array_of_names(tag_names)
     tag_names.map do |name|
       Tag.find_or_create_by(text: name)
-    end
-  end
-
-  private
-  def text_should_be_present_and_unique
-    if text.blank? or Tag.pluck(:text).map(&:upcase).include?(text.upcase)
-      errors.add(:text, "should be present and unique")
     end
   end
 end
