f7c71972c845be5fea9c7430ec78823e3d8fec20
jennyshih <jinghua.shih@gmail.com>
2020-11-30 11:24:52 +0800

    [Feat] Initialize Elasticsearch config in local env only

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,5 @@
-Post.__elasticsearch__.client = Elasticsearch::Client.new(log: true, url: 'http://localhost:9201')
-Post.__elasticsearch__.create_index!
-Post.import
+if Rails.env.development? || Rails.env.test?
+  Post.__elasticsearch__.client = Elasticsearch::Client.new(log: true, url: 'http://localhost:9201')
+  Post.__elasticsearch__.create_index!
+  Post.__elasticsearch__.import
+end
