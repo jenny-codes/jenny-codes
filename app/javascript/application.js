3caffc3b57fcdee495bb5469a28de79672183186
Jenny Shih <jenny.shih@shopify.com>
2025-10-27 09:11:54 -0400

    Refactor: Merge cleanBlog.js ino application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,8 +6,55 @@ window.jQuery = $;
 import "bootstrap/dist/js/bootstrap.bundle";
 import "jquery-lazy";
 
-import { initializeCleanBlog } from "./clean_blog";
 import "./vendor/prism";
+
+const setupFloatingLabels = () => {
+  $("body")
+    .on("input propertychange", ".floating-label-form-group", (event) => {
+      const hasValue = Boolean($(event.target).val());
+      $(event.currentTarget).toggleClass("floating-label-form-group-with-value", hasValue);
+    })
+    .on("focus", ".floating-label-form-group", (event) => {
+      $(event.currentTarget).addClass("floating-label-form-group-with-focus");
+    })
+    .on("blur", ".floating-label-form-group", (event) => {
+      $(event.currentTarget).removeClass("floating-label-form-group-with-focus");
+    });
+};
+
+const setupNavbarScrollBehavior = () => {
+  const breakpoint = 992;
+
+  if ($(window).width() <= breakpoint) {
+    return;
+  }
+
+  const headerHeight = 66;
+
+  $(window).on(
+    "scroll",
+    { previousTop: 0 },
+    function onScroll() {
+      const currentTop = $(window).scrollTop();
+
+      if (currentTop < this.previousTop) {
+        if (currentTop > 0 && $("#mainNav").hasClass("is-fixed")) {
+          $("#mainNav").addClass("is-visible");
+        } else {
+          $("#mainNav").removeClass("is-visible is-fixed");
+        }
+      } else if (currentTop > this.previousTop) {
+        $("#mainNav").removeClass("is-visible");
+
+        if (currentTop > headerHeight && !$("#mainNav").hasClass("is-fixed")) {
+          $("#mainNav").addClass("is-fixed");
+        }
+      }
+
+      this.previousTop = currentTop;
+    }
+  );
+};
 
 const initializeLazyImages = () => {
   $(".lazy").Lazy({
@@ -19,7 +66,8 @@ const initializeLazyImages = () => {
 };
 
 export const bootstrapApplication = () => {
-  initializeCleanBlog();
+  setupFloatingLabels();
+  setupNavbarScrollBehavior();
   initializeLazyImages();
 };
 
